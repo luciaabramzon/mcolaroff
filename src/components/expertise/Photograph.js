d4475c4f257fee92b1cf14d2b22e2aecd39948d9
Lucia Abramzon <luciaabramzon@192.168.1.16>
2023-08-22 18:24:56 -0300

    ph

--- src/components/expertise/Photograph.js
+++ src/components/expertise/Photograph.js
@@ -2,10 +2,25 @@ import '../../styles/uxui.css'
 import '../../styles/animation.css'
 import NavExpertise from './NavExpertise'
 import cuadrado from '../../images/cuadradoLoader.svg'
-import ProjectRight from './ProjectRight'
-import projectR from '../../images/projectR.svg'
-import ProjectLeft from './ProjectLeft'
-import projectL from '../../images/projectL.svg'
+import p1 from '../../images/p1.webp'
+import p2 from '../../images/p2.webp'
+import p3 from '../../images/p3.webp'
+import p4 from '../../images/p4.webp'
+import p5 from '../../images/p5.webp'
+import p6 from '../../images/p6.webp'
+import p7 from '../../images/p7.webp'
+import p8 from '../../images/p8.webp'
+import p9 from '../../images/p9.webp'
+import p10 from '../../images/p10.webp'
+import p11 from '../../images/p11.webp'
+import p12 from '../../images/p12.webp'
+import p13 from '../../images/p13.webp'
+import p14 from '../../images/p14.webp'
+import p15 from '../../images/p15.webp'
+import p16 from '../../images/p16.webp'
+import p17 from '../../images/p17.webp'
+import p18 from '../../images/p18.webp'
+import p19 from '../../images/p19.webp'
 import Footer from '../Footer'
 import { useLocation } from 'react-router-dom'
 import { useEffect, useState } from 'react'
@@ -65,33 +80,77 @@ return(
                
             </div>
         </div>
-         <div className='projects'>
-                <ProjectLeft
-                numberLeft='02'
-                 projectLeftName="Name of the proyect"
-                 projectLeftText="Breif information about the proyect which problems  I solve as a designer."
-                 buttonLeftUrl="/project-1"
-                 projectLeftImage={projectL}
-                 />
-         </div>
-         <div className='projects'>
-                <ProjectRight
-                number='01'
-                 projectName="Name of the proyect"
-                 projectText="Breif information about the proyect which problems  I solve as a designer."
-                 buttonUrl="/project-1"
-                 projectImage={projectR}
-                 />
-         </div>
-         <div className='projects'>
-                <ProjectLeft
-                numberLeft='02'
-                 projectLeftName="Name of the proyect"
-                 projectLeftText="Breif information about the proyect which problems  I solve as a designer."
-                 buttonLeftUrl="/project-1"
-                 projectLeftImage={projectL}
-                 />
-         </div>
+        <div className='passionPhotos'>
+                <div className='passions'>
+                <div className='passionsImgContainer'>
+                    <img className='passionsImg' src={p1}/>
+                    </div>
+                    <div className='passionsImgContainer'>
+                    <img className='passionsImg' src={p2}/>
+                    </div>
+                    <div className='passionsImgContainer'>
+                    <img className='passionsImg' src={p3}/>
+                    </div>
+                </div>
+                <div className='passions'>
+                <div className='passionsImgContainer'>
+                    <img className='passionsImg' src={p4}/>
+                    </div>
+                    <div className='passionsImgContainer'>
+                    <img className='passionsImg' src={p5}/>
+                    </div>
+                    <div className='passionsImgContainer'>
+                    <img className='passionsImg' src={p6}/>
+                    </div>
+                </div>
+                <div className='passions'>
+                <div className='passionsImgContainer'>
+                    <img  className='passionsImg' src={p7}/>
+                    </div>
+                    <div className='passionsImgContainer'>
+                    <img className='passionsImg' src={p8}/>
+                    </div>
+                    <div className='passionsImgContainer'>
+                    <img className='passionsImg' src={p9}/>
+                    </div>
+                    <div className='passionsImgContainer'>
+                    <img className='passionsImg' src={p10}/>
+                    </div>
+                </div>
+                <div className='passions'>
+                <div className='passionsImgContainer'>
+                    <img className='passionsImg' src={p11}/>
+                    </div>
+                    <div className='passionsImgContainer'>
+                    <img className='passionsImg' src={p12}/>
+                    </div>
+                    <div className='passionsImgContainer'>
+                    <img className='passionsImg'  src={p13}/>
+                    </div>
+                </div>
+                <div className='passions'>
+                <div className='passionsImgContainer'>
+                    <img className='passionsImg' src={p14}/>
+                    </div>
+                    <div className='passionsImgContainer'>
+                    <img className='passionsImg' src={p15}/>
+                    </div>
+                    <div className='passionsImgContainer'>
+                    <img className='passionsImg' src={p16}/>
+                    </div>
+                </div>
+                <div className='passionsLast'>
+                <div className='passionsImgContainer'>
+                    <img className='passionsImg' src={p17}/>
+                    </div>
+                    <div className='passionsImgContainer'>
+                    <img className='passionsImg' src={p18}/>
+                    </div>
+                    <div className='passionsImgContainer'>
+                    <img className='passionsImg' src={p19}/>
+                    </div>
+                </div>
+            </div>
          <div className='footerUx' id='footer'> 
             <Footer/>
          </div>
